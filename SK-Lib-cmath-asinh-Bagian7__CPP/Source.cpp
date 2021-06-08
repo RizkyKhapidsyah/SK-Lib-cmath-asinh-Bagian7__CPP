463cbb4801a1cffccc9923c6c05dcdaad4684fb1
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-08 19:39:24 +0700

    Add project files.

---
+++ SK-Lib-cmath-asinh-Bagian7__CPP/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*  Source by CPPReference
+    Modified For Learn by Rizky Khapidsyah
+    I.D.E : VS2019 */
+
+int main() {
+    std::cout << "asinh(1) = " << std::asinh(1) << '\n' << "asinh(-1) = " << std::asinh(-1) << '\n';
+    // special values
+    std::cout << "asinh(+0) = " << std::asinh(+0.0) << '\n' << "asinh(-0) = " << std::asinh(-0.0) << '\n';
+
+    _getch();
+    return 0;
+}
